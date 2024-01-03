9649bfb708d53ff943f7d962bb1678c7ecf3765c
almaz <almazgimaev@gmail.com>
2024-01-03 12:31:10 +0100

    update app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os
+
 from dotenv import load_dotenv
+
 import supervisely as sly
 
-# for convenient debug, has no effect in production
 if sly.is_development():
     load_dotenv("local.env")
     load_dotenv(os.path.expanduser("~/supervisely.env"))
@@ -12,25 +13,26 @@ import functions as f
 import globals as g
 
 
+@g.my_app.callback("import_images")
 @sly.timeit
-def import_images(api: sly.Api, task_id: int):
+def import_images(api: sly.Api, task_id, context, state, app_logger):
     dir_info = api.file.list(g.TEAM_ID, g.INPUT_PATH)
     if len(dir_info) == 0:
         raise Exception(f"There are no files in selected directory: '{g.INPUT_PATH}'")
 
-    sly.logger.debug(f"Number of files in selected directory: {len(dir_info)}")
+    app_logger.debug(f"Number of files in selected directory: {len(dir_info)}")
 
     if len(dir_info) == 1:
-        sly.logger.debug(
+        app_logger.debug(
             f"There is only one file in directory {g.INPUT_PATH}. "
             "Will check if it is an archive or an image."
         )
         file_name = dir_info[0].get("name")
         file_ext = sly.fs.get_file_ext(file_name)
         if file_ext in g.SUPPORTED_IMG_EXTS:
-            sly.logger.debug(f"File {file_name} is an image.")
+            app_logger.debug(f"File {file_name} is an image.")
         else:
-            sly.logger.debug(
+            app_logger.debug(
                 f"File {file_name} is not an image, will try to handle it as an archive."
             )
             dir_info = f.unpack_archive_on_team_files(api, dir_info[0].get("path"))
@@ -42,7 +44,7 @@ def import_images(api: sly.Api, task_id: int):
             else g.OUTPUT_PROJECT_NAME
         )
 
-        sly.logger.debug(f"Project name: {project_name}")
+        app_logger.info(f"Project name: {project_name}")
 
         project = api.project.create(
             workspace_id=g.WORKSPACE_ID, name=project_name, change_name_if_conflict=True
@@ -51,7 +53,7 @@ def import_images(api: sly.Api, task_id: int):
         project = api.project.get_info_by_id(g.PROJECT_ID)
 
     if g.NEED_DOWNLOAD:
-        sly.logger.info(f"Data will be downloaded: {g.INPUT_PATH}")
+        app_logger.info(f"Data will be downloaded: {g.INPUT_PATH}")
         f.download_project(api, g.INPUT_PATH)
 
     dataset_info = None
@@ -61,8 +63,8 @@ def import_images(api: sly.Api, task_id: int):
     else:
         datasets_names, datasets_images_map = f.get_datasets_images_map(dir_info, None)
 
-    sly.logger.debug(f"Datasets names: {datasets_names}")
-    sly.logger.debug(f"Datasets images map: {datasets_images_map}")
+    app_logger.info(f"Datasets names: {datasets_names}")
+    app_logger.info(f"Datasets images map: {datasets_images_map}")
 
     for dataset_name in datasets_names:
         if g.DATASET_ID is None:
@@ -108,7 +110,7 @@ def import_images(api: sly.Api, task_id: int):
                         hashes=batch_hashes,
                     )
                 except Exception as e:
-                    sly.logger.warn(msg=e)
+                    app_logger.warn(msg=e)
 
             progress.iters_done_report(len(batch_names))
 
@@ -117,12 +119,13 @@ def import_images(api: sly.Api, task_id: int):
     if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
         api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_PATH)
         source_dir_name = g.INPUT_PATH.lstrip("/").rstrip("/")
-        sly.logger.info(msg=f"Source directory: '{source_dir_name}' was successfully removed.")
+        app_logger.info(msg=f"Source directory: '{source_dir_name}' was successfully removed.")
 
     api.task.set_output_project(task_id=task_id, project_id=project.id, project_name=project.name)
+    g.my_app.stop()
 
 
-if __name__ == "__main__":
+def main():
     sly.logger.info(
         "Script arguments",
         extra={
@@ -132,8 +135,13 @@ if __name__ == "__main__":
         },
     )
 
-    import_images(g.api, g.TASK_ID)
     try:
-        sly.app.fastapi.shutdown()
-    except KeyboardInterrupt:
-        sly.logger.info("Application shutdown successfully")
+        g.my_app.run(initial_events=[{"command": "import_images"}])
+    finally:
+        if not sly.is_development():
+            sly.logger.info(f"Remove data directory: {g.STORAGE_DIR}")
+            sly.fs.remove_dir(g.STORAGE_DIR)
+
+
+if __name__ == "__main__":
+    sly.main_wrapper("main", main, log_for_agent=False)
