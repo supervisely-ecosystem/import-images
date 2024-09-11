398744418f3277527ac009ae9792f8ae0fbcb2e3
vorozhkog <vorozkhog@icloud.com>
2024-09-11 11:10:05 +0100

    change variable name

--- src/functions.py
+++ src/functions.py
@@ -18,17 +18,17 @@ import globals as g
 register_heif_opener()
 
 def get_project_name() -> str:
-    n = g.OUTPUT_PROJECT_NAME
-    if len(n) == 0:
+    project_name = g.OUTPUT_PROJECT_NAME
+    if len(project_name) == 0:
         full_path_dir = os.path.dirname(g.INPUT_PATH)
         return os.path.basename(full_path_dir) or sly.fs.get_file_name(g.INPUT_PATH)
-    if '/' in n or "|" in n or '\\' in n:
+    if any(char in project_name for char in ['/', '|', '\\']):
         sly.logger.warning('Project name you have provided is invalid. '
                       'Project and dataset names cannot contain following characters: "\\", "/", "|". '
-                      'Thus, destination project will not contain them.', extra={'input name': n})
-        n = n.replace('/', '').replace('|', '').replace('\\', '')
-    sly.logger.debug(f"Project name: {n}")
-    return n
+                      'Thus, destination project will not contain them.', extra={'input name': project_name})
+        project_name = project_name.replace('/', '').replace('|', '').replace('\\', '')
+    sly.logger.debug(f"Project name: {project_name}")
+    return project_name
 
 
 def get_project_name_from_input_path(input_path: str) -> str:
